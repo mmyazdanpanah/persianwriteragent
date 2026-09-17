413b9d14012817926a43101bbfc7d93ffb36f350
Mohammad Mostafa Yazdanpanah <mohammadmostafayazdanpanah@gmail.com>
2026-09-17 16:13:59 +0330

    feat(persian): enable Hazm for Persian writing

--- plugin/scripting/sandbox.py
+++ plugin/scripting/sandbox.py
@@ -143,6 +143,8 @@ VENV_AUTHORIZED_IMPORTS: tuple[str, ...] = (
     "plugin.scripting.text_analytics",  # trusted text analytics (spaCy) for Run Python Script + direct imports in user scripts
     "spacy",
     "spacy.*",
+    "hazm",
+    "hazm.*",
     "textdescriptives",
     "spacytextblob",
     "spacytextblob.*",
